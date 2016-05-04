a94bd29bb1554ad12eddca608a1d7e763d393dc7
mikeseaflow <miksjob@student.chalmers.se>
2016-05-04 08:14:29 +0200

    Liten uppdatering. Tog bort gammal kod och uppdaterade Databasen med en kolumn för status.

--- app/src/main/java/crispit/errorextractor/DBHelper.java
+++ app/src/main/java/crispit/errorextractor/DBHelper.java
@@ -12,7 +12,7 @@ package crispit.errorextractor;
  */
 public class DBHelper extends SQLiteOpenHelper{
 
-    //Oföränderliga strängar som används till skapande av databasen
+    //Ofˆr‰nderliga str‰ngar som anv‰nds till skapande av databasen
     public static final String TABLE_NAME = "ErrorReport";
     public static final String COLUMN_NAME_ENTRYID = "ErrorID";
     public static final String COLUMN_NAME_SYMPTOM = "Symptom";
@@ -20,6 +20,7 @@ public class DBHelper extends SQLiteOpenHelper{
     public static final String COLUMN_NAME_DATE = "Date";
     public static final String COLUMN_NAME_COMMENT = "Comment";
     public static final String COLUMN_NAME_GRADE = "Grade";
+    public static final String COLUMN_NAME_STATUS = "Status";
 
     //Busspecifik data
     public static final String COLUMN_NAME_Accelerator_Pedal_Position = "Accelerator_Pedal_Position";
@@ -55,33 +56,7 @@ public class DBHelper extends SQLiteOpenHelper{
     private static final String COMMA_SEP = ",";
     private static final String NUMBER_TYPE = " INTEGER";
 
-    /*Ericsson$Accelerator_Pedal_Position - Gaspedalens position
-    Ericsson$Ambient_Temperature - Lufttemperaturen utanför bussen
-    Ericsson$At_Stop - Bussen står på en hållplats
-    Ericsson$Cooling_Air_Conditioning - Instrumentpanelens info om luftkonditioneringen
-    Ericsson$Driver_Cabin_Temperature - Lufttemperatur inne i förarhytten
-    Ericsson$Fms_Sw_Version_Supported - FMS-version som fordonet stöder
-    Ericsson$GPS - GPS-position från Västtrafik
-    Ericsson$GPS2 - GPS-position från Icomera X6
-    Ericsson$GPS_NMEA - GPS-position från Keolis (rmc-format)
-    Ericsson$Journey_Info - Information om turen
-    Ericsson$Mobile_Network_Cell_Info - Mobilnätets cellinfo för Keolis WAN-länk
-    Ericsson$Mobile_Network_Signal_Strength - Mobilsignalstyrka för Keolis WAN-länk
-    Ericsson$Next_Stop - Nästa hållplats
-    Ericsson$Offroute - Fordonet avviker från sin rutt
-    Ericsson$Online_Users - Antalet användare som är uppkopplade på WiFi-nätet
-    Ericsson$Opendoor - Är någon av bussens dörrar öppna
-    Ericsson$Position_Of_Doors - Indikerar aktuell position på dörrarna
-    Ericsson$Pram_Request - Instrumentpanelens info om barnvagnsknappen
-    Ericsson$Ramp_Wheel_Chair_Lift - Positionen på ramp/lift
-    Ericsson$Status_2_Of_Doors - Är dörrarna aktiverade för automatisk öppning/stängning
-    Ericsson$Stop_Pressed - Någon har tryckt på stoppknappen
-    Ericsson$Stop_Request - Instrumentpanelens info om stoppknappen
-    Ericsson$Total_Vehicle_Distance - Bussens totala körsträcka
-    Ericsson$Turn_Signals - Instrumentpanelens info om blinkersljus
-    Ericsson$Wlan_Connectivity - Information om wifi-uppkopplingen*/
-
-    //Sträng för att skapa vår databas i SQL
+    //String to create table in DB
     private static final String SQL_CREATE_ENTRIES = " CREATE TABLE " + TABLE_NAME +
             "(" + COLUMN_NAME_ENTRYID + " TEXT PRIMARY KEY, " +
             COLUMN_NAME_SYMPTOM + TEXT_TYPE +  COMMA_SEP +
@@ -89,6 +64,7 @@ public class DBHelper extends SQLiteOpenHelper{
             COLUMN_NAME_BUSID + TEXT_TYPE  + COMMA_SEP +
             COLUMN_NAME_DATE + TEXT_TYPE  + COMMA_SEP +
             COLUMN_NAME_GRADE + NUMBER_TYPE + COMMA_SEP +
+            COLUMN_NAME_STATUS + NUMBER_TYPE + COMMA_SEP +
             COLUMN_NAME_Accelerator_Pedal_Position + TEXT_TYPE + COMMA_SEP +
             COLUMN_NAME_Ambient_Temperature + TEXT_TYPE + COMMA_SEP +
             COLUMN_NAME_At_Stop + TEXT_TYPE + COMMA_SEP +
@@ -115,11 +91,11 @@ public class DBHelper extends SQLiteOpenHelper{
             COLUMN_NAME_Turn_Signals + TEXT_TYPE + COMMA_SEP +
             COLUMN_NAME_Wlan_Connectivity + TEXT_TYPE + ")"  ;
 
-    //Sträng för att droppa vår databas i SQL
+    //Str‰ng fˆr att droppa vÂr databas i SQL
     private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS" +
             TABLE_NAME;
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "Database.db";
 
     //konstruktor
@@ -138,7 +114,7 @@ public class DBHelper extends SQLiteOpenHelper{
     }
 
     /**
-     * Metod för upgradering av databasen
+     * Metod fˆr upgradering av databasen
      */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
@@ -147,7 +123,7 @@ public class DBHelper extends SQLiteOpenHelper{
     }
 
     /**
-     * Metod för insättning av felrapporter i databasen
+     * Metod fˆr ins‰ttning av felrapporter i databasen
      * @param errorID
      * @param symptom
      * @param busID
@@ -156,7 +132,7 @@ public class DBHelper extends SQLiteOpenHelper{
      * @param grade
      * @return
      */
-    public boolean insertErrorReport(String errorID, String symptom, String comment, String busID, String date, int grade,
+    public boolean insertErrorReport(String errorID, String symptom, String comment, String busID, String date, int grade, String status,
                                      String accelerator_Pedal_Position, String ambient_Temperature, String at_Stop, String cooling_Air_Conditioning,
                                      String driver_Cabin_Temperature, String fms_Sw_Version_Supported, String gps, String gps2,
                                      String gps_nmea, String journey_Info, String mobile_Network_Cell_Info, String mobile_Network_Signal_Strength,
@@ -171,6 +147,7 @@ public class DBHelper extends SQLiteOpenHelper{
         contentValues.put("BusID", busID);
         contentValues.put("Date", date);
         contentValues.put("Grade", grade);
+        contentValues.put("Status", status);
         contentValues.put("Accelerator_Pedal_Position", accelerator_Pedal_Position);
         contentValues.put("Ambient_Temperature", ambient_Temperature);
         contentValues.put("At_Stop", at_Stop);
@@ -202,7 +179,7 @@ public class DBHelper extends SQLiteOpenHelper{
     }
 
     /**
-     * Metod för att hitta en felrapport med ett specifikt errorId
+     * Metod fˆr att hitta en felrapport med ett specifikt errorId
      * @param id
      * @return en cursor
      */
@@ -230,8 +207,8 @@ public class DBHelper extends SQLiteOpenHelper{
     }
 
     /**
-     * Metod för att hitta alla felraporten för en specifik buss
-     * @param busID id för att identifiera en specifik buss
+     * Metod fˆr att hitta alla felraporten fˆr en specifik buss
+     * @param busID id fˆr att identifiera en specifik buss
      */
     public ArrayList<ErrorReport> getBusReports(String busID) {
         ArrayList<ErrorReport> array_list = new ArrayList<>();
@@ -257,7 +234,7 @@ public class DBHelper extends SQLiteOpenHelper{
 
 
     /**
-     * Metod för att returnera alla unika bussar (med fel)
+     * Metod fˆr att returnera alla unika bussar (med fel)
      * @return arraylist med bussar som har felrapporter
      */
     public ArrayList<String> getAllBuses() {
@@ -273,6 +250,24 @@ public class DBHelper extends SQLiteOpenHelper{
             res.moveToNext();
         }
         return array_list;
+    }
+
+    public String getNewErrorId(){
+
+        int nextId=0;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select ErrorID from ErrorReport", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            int currentId = Integer.parseInt(res.getString(res.getColumnIndex(COLUMN_NAME_ENTRYID)));
+            if(currentId>nextId)
+                nextId=currentId;
+            res.moveToNext();
+        }
+
+        return Integer.toString(nextId+1);
     }
 
 }
--- app/src/main/java/crispit/errorextractor/MainActivity.java
+++ app/src/main/java/crispit/errorextractor/MainActivity.java
@@ -23,9 +23,7 @@ public class MainActivity extends AppCompatActivity {
         ListView busList = new ListView(this.getBaseContext());
         busId = "Vin_Num_001";
         mydb = new DBHelper(this);
-        //new RetrieveBusData().execute("Test");
-        //mydb.insertErrorReport("2","Trasig Motor","Fungerar en del.","Buss1","2016-02-22,10:00",4);
-        //mydb.insertErrorReport("3","Trasigt Däck","Total paj.","Buss2","2016-04-22,15:22",5);
+
         listView = (ListView) findViewById(R.id.busList);
 
         list=new ArrayList<>();
@@ -53,37 +51,4 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(objAdapter);
     }
 
-    private class RetrieveBusData extends AsyncTask<String, String, String> {
-
-        private Exception exception;
-
-        protected String doInBackground(String... str) {
-            try {
-
-                mydb.insertErrorReport("5", "Gasen funkar!!", "OMG WEEY!!", "Vin_Num_001", "2016-02-25,05:00", 1,BusData.getBusInfo(busId,"Accelerator_Pedal_Position"),
-                        BusData.getBusInfo(busId,"Ambient_Temperature"), BusData.getBusInfo(busId,"At_Stop"),
-                        BusData.getBusInfo(busId,"Cooling_Air_Conditioning"), BusData.getBusInfo(busId,"Driver_Cabin_Temperature"),
-                        BusData.getBusInfo(busId,"Fms_Sw_Version_Supported"), BusData.getBusInfo(busId,"GPS"),
-                        BusData.getBusInfo(busId,"GPS2"), BusData.getBusInfo(busId,"GPS_NMEA"),
-                        BusData.getBusInfo(busId,"Journey_Info"), BusData.getBusInfo(busId,"Mobile_Network_Cell_Info"),
-                        BusData.getBusInfo(busId,"Mobile_Network_Signal_Strength"), BusData.getBusInfo(busId,"Next_Stop"),
-                        BusData.getBusInfo(busId,"Offroute"), BusData.getBusInfo(busId,"Online_Users"),
-                        BusData.getBusInfo(busId,"Opendoor"), BusData.getBusInfo(busId,"Position_Of_Doors"),
-                        BusData.getBusInfo(busId,"Pram_Request"), BusData.getBusInfo(busId,"Ramp_Wheel_Chair_Lift"),
-                        BusData.getBusInfo(busId,"Status_2_Of_Doors"), BusData.getBusInfo(busId,"Stop_Pressed"),
-                        BusData.getBusInfo(busId,"Stop_Request"), BusData.getBusInfo(busId,"Total_Vehicle_Distance"),
-                        BusData.getBusInfo(busId,"Turn_Signals"), BusData.getBusInfo(busId,"Wlan_Connectivity"));
-
-            } catch (Exception e) {
-                this.exception = e;
-                return "Could not insert!";
-            }
-            return "Insertion successful!";
-        }
-
-        protected void onPostExecute() {
-            // TODO: check this.exception
-            // TODO: do something with the feed
-        }
-    }
 }
